73e559365f143410ccb566381660ac0591617de3
Lo0syash <arturdzhan2004@gmail.com>
2023-01-08 01:46:11 +0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
-function fun(){
-    range__input.innerHTML = range.value;
+var element = 5;
+
+function generateColor(){
+    for (var i = 0; i < element; i++){
+        var hex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+        var hex__color = "";
+        for(var j = 0; j < 6; j++){
+            var random__index = Math.floor(Math.random()*hex.length)
+            hex__color += hex[random__index];
+        }
+        var colorItem = document.getElementsByClassName('color-item');
+        console.log(hex__color)
+        colorItem[i].style.backgroundColor = '#' + hex__color;
+    }
 }
 
-function current(){
-    var click=0;
-    if (click != 5){
-        console.log(click++)
-        changeColor();
+function addBlock(){
+    if(element != 5 && element < 5){
+        element++;
+
     }
     else{
-        alert('Достигнут лимит')
+        alert('Лимит')
     }
 }
 
-function changeColor(){
-    if (range.value > 0){
-        document.querySelector('.color-list').innerHTML += '<li class="color-item"></li>';
-    }
-    else{
-        alert('Выберите диапазон')
-    }
+
+function deleteBlock(e){
+    var array = [document.querySelector('.color-list')];
+    array.pop();
+
 }
+
